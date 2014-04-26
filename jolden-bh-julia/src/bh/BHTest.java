3571bf3f9170f26cd69fc76d6ca130a0dc1a3ff2
Mohsen Vakilian <reprogrammer@gmail.com>
2014-04-26 12:47:24 -0500

    Imported the jolden-bh project.

---
+++ jolden-bh-julia/src/bh/BHTest.java
@@ -0,0 +1,34 @@
+package bh;
+
+import static org.junit.Assert.*;
+
+import java.util.Enumeration;
+
+import org.junit.Test;
+
+public class BHTest {
+
+  static double DTIME = 0.0125;
+  private static double TSTOP = 2.0;
+
+  @Test
+  public void test() {
+    int nsteps = 10;
+    int nbody = 5;
+    Tree root = new Tree();
+    root.createTestData(nbody);
+    double tnow = 0.0;
+    int i = 0;
+    while ((tnow < TSTOP + 0.1 * DTIME) && (i < nsteps)) {
+      root.stepSystem(i++);
+      tnow += DTIME;
+    }
+    StringBuilder sb = new StringBuilder();
+    for (Enumeration e = root.bodies(); e.hasMoreElements();) {
+      Body b = (Body) e.nextElement();
+      sb.append(b.pos + ",");
+    }
+    assertEquals("-1 -1 -1 ,2 0 0 ,0 1 2 ,1 -1 -1 ,-1 1 0 ,", sb.toString());
+  }
+
+}
