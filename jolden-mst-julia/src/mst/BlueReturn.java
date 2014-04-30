8ff01a88d2fcc169336b12c942f990d98d498c28
Mohsen Vakilian <reprogrammer@gmail.com>
2014-04-29 21:48:44 -0500

    Promoted BlueReturn to a new file. issue #8

---
+++ jolden-mst-julia/src/mst/BlueReturn.java
@@ -0,0 +1,23 @@
+package mst;
+
+class BlueReturn {
+  private Vertex vert;
+  private int dist;
+
+  public Vertex vert() {
+    return vert;
+  }
+
+  public void setVert(Vertex v) {
+    vert = v;
+  }
+
+  public int dist() {
+    return dist;
+  }
+
+  public void setDist(int d) {
+    dist = d;
+  }
+
+}
--- jolden-mst-julia/src/mst/MST.java
+++ jolden-mst-julia/src/mst/MST.java
@@ -172,29 +172,3 @@ public class MST {
   }
 
 }
-
-
-/**
- * Not really sure what this is for?
- **/
-class BlueReturn {
-  private Vertex vert;
-  private int dist;
-
-  public Vertex vert() {
-    return vert;
-  }
-
-  public void setVert(Vertex v) {
-    vert = v;
-  }
-
-  public int dist() {
-    return dist;
-  }
-
-  public void setDist(int d) {
-    dist = d;
-  }
-
-}
