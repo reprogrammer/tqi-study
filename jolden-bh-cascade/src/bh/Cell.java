0c87bd510ffe1b8295de79fa14d60e31826233fb
Mohsen Vakilian <reprogrammer@gmail.com>
2014-04-30 10:44:42 -0500

    Added BH for cascade.

---
+++ jolden-bh-cascade/src/bh/Cell.java
@@ -0,0 +1,50 @@
+package bh;
+
+/**
+ * A class used to represent internal nodes in the tree
+ **/
+public final class Cell extends Node {
+  // subcells per cell
+  public final static int NSUB = 8; // 1 << NDIM
+
+  /**
+   * The children of this cell node. Each entry may contain either another cell or a body.
+   **/
+  Node[] subp;
+  Cell next;
+
+  public Cell() {
+    subp = new Node[NSUB];
+    next = null;
+  }
+
+  /**
+   * Descend Tree and insert particle. We're at a cell so we need to move down the tree.
+   * 
+   * @param p the body to insert into the tree
+   * @param xpic
+   * @param l
+   * @param tree the root of the tree
+   * @return the subtree with the new body inserted
+   **/
+  public final Node loadTree(Body p, MathVector xpic, int l, Tree tree) {
+    // move down one level
+    int si = oldSubindex(xpic, l);
+    Node rt = subp[si];
+    if (rt != null)
+      subp[si] = rt.loadTree(p, xpic, l >> 1, tree);
+    else
+      subp[si] = p;
+    return this;
+  }
+
+  /**
+   * Return a string representation of a cell.
+   * 
+   * @return a string representation of a cell.
+   **/
+  public String asString() {
+    return "Cell " + super.toString();
+  }
+
+}
---
+++ jolden-bh-cascade/src/bh/Node.java
@@ -0,0 +1,49 @@
+package bh;
+
+/**
+ * A class that represents the common fields of a cell or body data structure.
+ **/
+public abstract class Node {
+  /**
+   * Mass of the node.
+   **/
+  double mass;
+  /**
+   * Position of the node
+   **/
+  MathVector pos;
+
+  // highest bit of int coord
+  static final int IMAX = 1073741824;
+
+  // potential softening parameter
+  static final double EPS = 0.05;
+
+  /**
+   * Construct an empty node
+   **/
+  protected Node() {
+    mass = 0.0;
+    pos = new MathVector();
+  }
+
+  abstract Node loadTree(Body p, MathVector xpic, int l, Tree root);
+
+  public static final int oldSubindex(MathVector ic, int l) {
+    int i = 0;
+    for (int k = 0; k < MathVector.NDIM; k++) {
+      if (((int) ic.value(k) & l) != 0) i += Cell.NSUB >> (k + 1);
+    }
+    return i;
+  }
+
+  /**
+   * Return a string representation of a node.
+   * 
+   * @return a string representation of a node.
+   **/
+  public String asString() {
+    return mass + " : " + pos;
+  }
+
+}
